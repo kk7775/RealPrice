3878897b155f5ece2e2ec69bb90f26a33e5256fc
kk7775 <42362964+kk7775@users.noreply.github.com>
2019-01-22 11:22:56 +0800

    update v2

--- RealPrice_v2.py
+++ RealPrice_v2.py
@@ -6,9 +6,9 @@ import numpy             as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets, linear_model
 
-area   = ['中西區', '北區', '東區']
+area   = ['北區', '東區', '中西區','永康區']
 status = ['土地', '房地(土地+建物)+車位', '房地(土地+建物)']
-dir = os.path.dirname(os.path.realpath(__file__))
+dir    = os.path.dirname(os.path.realpath(__file__))
 
 # merge.csv
 file = os.path.join(dir,'merge.csv') 
@@ -50,20 +50,24 @@ def merge():
                             elif i[:2] in area: 
                                 add_to_csv(i)
 
-
-
+# 新增自csv                                
 def add_to_csv(content):
     with open(file,'a') as f:
         f.write(content)
         f.close()        
 
-# 讀取merge.csv，拿掉不要的欄位
+# 讀取merge.csv，拿掉不要的欄位，空的欄位用nah填滿然後都刪掉(去除無效的列)
 def step_1(name):
     s1 = os.path.join(dir, name)
     con_1 = ['鄉鎮市區','交易標的','土地移轉總面積(平方公尺)','建物型態','交易年月日','建物移轉總面積(平方公尺)','總價(元)','單價(元/平方公尺)']
     con_2 = ['交易年月日','鄉鎮市區','交易標的','單價(元/平方公尺)']
-    df = pd.read_csv(file,encoding='big5')
+    df = pd.read_csv(file,encoding='big5',low_memory=False)
     df = df[con_2]
+
+    # 去除無效的列
+    df['單價(元/平方公尺)'].replace('', np.nan, inplace=True)
+    df.dropna(subset=['單價(元/平方公尺)'], inplace=True)
+
     open(s1,'w')
     df.to_csv(s1, encoding='big5')
     return s1
@@ -80,6 +84,7 @@ def step_2(src_file ,name, area, status):
     #midwest   = df.loc[(df['鄉鎮市區'] == '中西區') & (df['交易標的'] == '房地(土地+建物)+車位')].to_csv(os.path.join(dir,'midwest.csv' ), columns=con_2, encoding='big5', index=False)
     return s2
 
+# 忘了...    
 def get_df(name):
     total_list = []
     need = ['交易年月日','單價(元/平方公尺)']
@@ -90,10 +95,9 @@ def get_df(name):
     df = df.dropna(axis=0,how='all')  
     df = df.dropna(axis=1,how='all')
     df = df.apply(lambda x: pd.to_numeric(x,errors='ignore')) 
-    
     return df
     
-# 106年有幾筆交易
+# 有幾筆交易
 def get_total(name):
     total_list = []
     need = ['交易年月日','單價(元/平方公尺)']
@@ -109,8 +113,8 @@ def get_total(name):
     total_106  = total_list.append(len(df.loc[(df['交易年月日'] == 106)].index))
     total_107  = total_list.append(len(df.loc[(df['交易年月日'] == 107)].index))
     return total_list
-    
-    
+
+# 平均價格    
 def get_mean(name):
     mean_list = []
     need = ['交易年月日','單價(元/平方公尺)']
@@ -134,120 +138,118 @@ def get_mean(name):
      
     return mean_list
 
-def plot(data, label):
-    plt.plot(index, north_y  , label = label)
-    plt.plot(index, midwest_y, label = 'midwset_y')
-    plt.plot(index, east_y   , label = 'east_y')
+# 畫圖
+def plot(index, data, label):
+    plt.plot(index, data  , label = label, marker='o')
     plt.xlabel('Year')
     plt.ylabel('Price')
     plt.grid(True)
     plt.legend(loc='best')
-    plt.title('Have Parking space')
     plt.xticks(index)
-    plt.show()
-    
-# 機器學習
+    plt.ylim((10, 25))
+
+# 機器學習(LinearRegression)
 def linearRe(df):
     from sklearn                  import datasets, linear_model
     from sklearn.preprocessing    import PolynomialFeatures
     from sklearn.cross_validation import train_test_split
     from sklearn.preprocessing    import StandardScaler
-
+    
+    regr        = linear_model.LinearRegression()
+    
     quadratic = PolynomialFeatures(degree=2)
 
-    # df = pd.read_csv('A_LVR_LAND_A.csv', encoding='big5')
-    X = df[['交易年月日']]
+    # df = df[['交易年月日']]
+    df = df[df['單價(元/平方公尺)']<50]
+    x = df[['交易年月日']]
     y = df[['單價(元/平方公尺)']]
 
-    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3) # 30% for testing, 70% for training
-    X_train.head()
+    # 切分訓練及測試資料
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3) # 30% for testing, 70% for training
 
-    X_train_raw = X_train.copy()
-    X_test_raw  = X_test.copy()
+    x_train_raw = x_train.copy()
+    x_test_raw  = x_test.copy()
+    
+    # 產生二次項係數
+    x_train     = quadratic.fit_transform(x_train_raw)
+    x_test      = quadratic.fit_transform(x_test_raw)
+    x_fit       = pd.DataFrame(np.arange(100, 108, 1))
+    
+    # 訓練
+    regr.fit(x_train, y_train)
+    
+    print("各變項參數: \n", regr.coef_)
+    print("均方誤差 (Mean squared error, MSE): %.2f" % np.mean((regr.predict(x_test) - y_test) ** 2))
 
-    X_train     = quadratic.fit_transform(X_train_raw)
-    X_test      = quadratic.fit_transform(X_test_raw)
+    
 
-    X_fit       = pd.DataFrame(np.arange(0, 0.1, 0.01))
-
-    regr        = linear_model.LinearRegression()
-
-    regr.fit(X_train, y_train)
-
-    #print('各變項參數: \n', regr.coef_)
-    #print("均方誤差 (Mean squared error, MSE): %.2f" % np.mean((regr.predict(X_test) - y_test) ** 2))
-
-
-    plt.scatter(X_test_raw, y_test,  color='blue', marker = 'x')
-    plt.plot(X_fit, regr.predict(quadratic.fit_transform(X_fit)), color='green', linewidth=1)
-
+    plt.scatter(x_test_raw, y_test,  color='blue', marker = 'x')
+    plt.plot(x_fit, regr.predict(quadratic.fit_transform(x_fit)), color='green', linewidth=1)
     plt.show()
     
 def main():
     index = [102, 103 , 104, 105, 106, 107]
+    label = ['north.csv','midwset.csv','east.csv','yongkong.csv']
 
-    
-    
     # init()
     # merge()  
     
-    # north_y   = get_mean(step_2(step_1('select_col.csv'), 'north.csv'  , area[0], status[1]))
-    # east_y    = get_mean(step_2(step_1('select_col.csv'), 'east.csv'   , area[1], status[1]))
-    # midwest_y = get_mean(step_2(step_1('select_col.csv'), 'midwest.csv', area[2], status[1]))
+    # linearRe(get_df('east.csv'))
+    # linearRe(get_df('midwest.csv'))
+    # linearRe(get_df('north.csv'))
+    
+    # 有車位 area   = ['北區', '東區', '中西區','永康區']
+    north_y     = get_mean(step_2(step_1('select_col.csv'),'north.csv'     , area[0], status[1]))
+    east_y      = get_mean(step_2(step_1('select_col.csv'),'east.csv'      , area[1], status[1]))
+    midwest_y   = get_mean(step_2(step_1('select_col.csv'),'midwest.csv'   , area[2], status[1]))
+    youngkong_y = get_mean(step_2(step_1('select_col.csv'),'yongkong.csv'  , area[3], status[1]))
+    
+    # 無車位
+    north_n     = get_mean(step_2(step_1('select_col.csv'), 'north.csv'    , area[0], status[2]))  
+    east_n      = get_mean(step_2(step_1('select_col.csv'), 'east.csv'     , area[1], status[2]))
+    midwest_n   = get_mean(step_2(step_1('select_col.csv'), 'midwest.csv'  , area[2], status[2]))
+    youngkong_n = get_mean(step_2(step_1('select_col.csv'), 'yongkong.csv' , area[3], status[2]))   
+    
+    plt.figure(figsize=(15, 5))
+    
+    plt.subplot(1, 3, 1)
+    plot(index, north_y    , label=label[0][0:-4])
+    plot(index, east_y     , label=label[1][0:-4])
+    plot(index, midwest_y  , label=label[2][0:-4])
+    plot(index, youngkong_y, label=label[3][0:-4])
+    plt.title('Have Parking space')
+    
+    plt.subplot(1, 3, 2)
+    plot(index, north_n    , label=label[0][0:-4])
+    plot(index, east_n     , label=label[1][0:-4])
+    plot(index, midwest_n  , label=label[2][0:-4])
+    plot(index, youngkong_n, label=label[3][0:-4])
+    plt.title('No Parking space')
+    
+    plt.subplot(1, 3, 3)
+    plt.plot(index, get_total('north.csv'  )   , label = 'north', marker='o')
+    for a, b in zip(index, get_total('north.csv'  )):
+        plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
+    
+    plt.plot(index, get_total('midwest.csv')   , label = 'midwest', marker='o')
+    for a, b in zip(index, get_total('midwest.csv'  )):
+        plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
 
-    # north_n   = get_mean(step_2(step_1('select_col.csv'), 'north.csv'  , area[0], status[2]))  
-    # east_n    = get_mean(step_2(step_1('select_col.csv'), 'east.csv'   , area[1], status[2]))
-    # midwest_n = get_mean(step_2(step_1('select_col.csv'), 'midwest.csv', area[2], status[2]))
+    plt.plot(index, get_total('east.csv'   )   , label = 'east', marker='o')
+    for a, b in zip(index, get_total('east.csv'  )):
+        plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
+   
+    plt.plot(index, get_total('yongkong.csv'   )   , label = 'youngkong', marker='o')
+    for a, b in zip(index, get_total('yongkong.csv'  )):
+        plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
     
-    # plt.rcParams['font.family'] = 'SimHei'
-    # plt.plot(index, north_y  , label = 'north_y')
-    # plt.plot(index, midwest_y, label = 'midwset_y')
-    # plt.plot(index, east_y   , label = 'east_y')
-    # plt.xlabel('Year')
-    # plt.ylabel('Price')
-    # plt.grid(True)
-    # plt.legend(loc='best')
-    # plt.title('Have Parking space')
-    # plt.xticks(index)
-    # plt.show()
+    plt.xlabel('Year')
+    plt.ylabel('count')
+    plt.grid(True)
+    plt.legend(loc='best')
+    plt.title('102~107')
+    plt.xticks(index)
 
-    
-    # plt.plot(index, midwest_n, label = 'midwset_n')
-    # plt.plot(index, north_n  , label = 'north_n')
-    # plt.plot(index, east_n   , label = 'east_n')  
-    # plt.xlabel('Year')
-    # plt.ylabel('Price')
-    # plt.grid(True)
-    # plt.legend(loc='best')
-    # plt.title('No Parking space')
-    # plt.xticks(index)
-    # plt.show()
-
-    # plt.plot(index, get_total('north.csv'  )   , label = 'north',marker='o')
-    # for a, b in zip(index, get_total('north.csv'  )):
-        # plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
-    
-    # plt.plot(index, get_total('east.csv'   )   , label = 'east',marker='o')
-    # for a, b in zip(index, get_total('east.csv'  )):
-        # plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
-    
-    # plt.plot(index, get_total('midwest.csv')   , label = 'midwest',marker='o')
-    # for a, b in zip(index, get_total('midwest.csv'  )):
-        # plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
-    
-    # plt.xlabel('Year')
-    # plt.ylabel('count')
-    # plt.grid(True)
-
-    # plt.legend(loc='best')
-    # plt.title('Turnover volume(accumulated to 107/06)')
-    # plt.xticks(index)
-    # plt.show()
-    
-    
-    
-    
-    
-    
-    
+    plt.show()
+        
 main()
